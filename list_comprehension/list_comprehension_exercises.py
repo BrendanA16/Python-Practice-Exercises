3046ef8a357ae3067cf209fe5036fd2327c56693
Brendan Arbuckle <brendan_arbuckle16@hotmail.com>
2024-03-15 11:24:48 -0400

    first commit

---
+++ list_comprehension/list_comprehension_exercises.py
@@ -0,0 +1,18 @@
+# Easy
+
+# Square Numbers: Create a list of squares for the numbers from 1 to 10.
+
+# Even Numbers: Generate a list of even numbers between 1 and 20.
+
+# Capitalize Words: Given a list of words, produce a new list with each word capitalized.
+
+# Medium
+
+# Filter Negative Numbers: Given a list of numbers, create a new list which contains every number 
+# in the given list which is positive.
+
+# Extract First Letter of Each Word: For a given list of words, create a list containing the first
+# letter of each word.
+
+# Square Even Numbers: Create a list of squares for the even numbers between 1 and 20. 
+# Use only one line of code.
